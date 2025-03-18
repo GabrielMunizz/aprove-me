dbd0bbd1ae234282f57539fce7516360a103acea
Gabriel Muniz <gabrielmfd@gmail.com>
2025-03-18 17:39:13 -0300

    Feat: generated integrations, assignor and payable modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma_service/prisma.service';
-import { TestController } from './test/test.controller';
+import { IntegrationsModule } from './integrations/integrations.module';
+import { PayableModule } from './payable/payable.module';
+import { AssignorModule } from './assignor/assignor.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TestController],
+  imports: [IntegrationsModule, PayableModule, AssignorModule],
+  controllers: [AppController],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
