d6c554033cf7cc016e8a9f5b86faa7fa27f994bb
Gabriel Muniz <gabrielmfd@gmail.com>
2025-03-20 16:07:44 -0300

    feat(user): implemented findUser test

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -4,6 +4,7 @@ import { PrismaService } from 'src/prisma_service/prisma.service';
 
 describe('UserService', () => {
   let service: UsersService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,9 +12,25 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('user.service', () => {
+    it('Should find a user', async () => {
+      const user = {
+        id: '1',
+        login: 'teste',
+        password: 'password',
+      };
+      jest.spyOn(prisma.users, 'findFirst').mockResolvedValue(user);
+
+      const result = await service.findOne('teste');
+
+      expect(result).toEqual(user);
+    });
   });
 });
