6133f5c678c98ab217a5376a9595b46fe3a32413
Gabriel Muniz <gabrielmfd@gmail.com>
2025-03-20 19:56:57 -0300

    feat(docker): configure docker to connect frontend and backend on different ports

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UsersService } from './user.service';
+import { PrismaService } from 'src/prisma_service/prisma.service';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -7,6 +9,7 @@ describe('UserController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [UsersService, PrismaService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { UsersService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('user')
+@Controller('user/')
 export class UserController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('signup')
+  async createUser(createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
+  }
+
   @Get()
   findAll() {
     return 'This action returns all users';
