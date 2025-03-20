091cfeb28351442d05efc1e29ebf4c8b478b9e00
Gabriel Muniz <gabrielmfd@gmail.com>
2025-03-20 12:44:00 -0300

    chore(auth): created auth test configurations

--- backend/src/assignor/assignor.controller.spec.ts
+++ backend/src/assignor/assignor.controller.spec.ts
@@ -1,17 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssignorController } from './assignor.controller';
 import { AssignorService } from './assignor.service';
+import { PrismaService } from 'src/prisma_service/prisma.service';
 
 describe('AssignorController', () => {
   let controller: AssignorController;
+  // let service: AssignorService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AssignorController],
-      providers: [AssignorService],
+      providers: [AssignorService, PrismaService],
     }).compile();
 
     controller = module.get<AssignorController>(AssignorController);
+    // service = module.get<AssignorService>(AssignorService);
   });
 
   it('should be defined', () => {
