43fac791c2e8ac7c94fe40544960a0bc8f30f220
Gabriel Muniz <gabrielmfd@gmail.com>
2025-03-20 16:36:22 -0300

    feat(auth): add auth.service tests

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -3,9 +3,12 @@ import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/user/user.service';
 import { PrismaService } from 'src/prisma_service/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let jwt: JwtService;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +16,34 @@ describe('AuthService', () => {
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    jwt = module.get<JwtService>(JwtService);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('auth.service', () => {
+    it('Should generate a token if login is successful', async () => {
+      const login = 'teste';
+      const password = 'teste';
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = {
+        id: '1',
+        login: 'teste',
+        password: hashedPassword,
+      };
+      const token = 'token';
+
+      jest.spyOn(userService, 'findOne').mockResolvedValue(user);
+
+      jest.spyOn(jwt, 'signAsync').mockResolvedValue(token);
+
+      const result = await service.signIn(login, password);
+
+      expect(result).toEqual({ accessToken: token });
+    });
   });
 });
