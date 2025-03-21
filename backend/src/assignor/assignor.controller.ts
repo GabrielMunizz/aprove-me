dd986e831402f17bed356d5d455d2bcc583e2f73
Gabriel Muniz <gabrielmfd@gmail.com>
2025-03-21 19:37:24 -0300

    feat(assignor): add Roles decorator to restrict user authorization

--- backend/src/assignor/assignor.controller.ts
+++ backend/src/assignor/assignor.controller.ts
@@ -12,27 +12,32 @@ import { AssignorService } from './assignor.service';
 import { CreateAssignorDto } from './dto/create-assignor.dto';
 import { UpdateAssignorDto } from './dto/update-assignor.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
 
 @UseGuards(AuthGuard)
 @Controller('integrations/')
 export class AssignorController {
   constructor(private readonly assignorService: AssignorService) {}
 
+  @Roles('admin')
   @Post('assignor')
   async createAssignor(@Body() createAssignorDto: CreateAssignorDto) {
     return await this.assignorService.create(createAssignorDto);
   }
 
+  @Roles('admin')
   @Get('assignor')
   async findAllAssignors() {
     return await this.assignorService.findAll();
   }
 
+  @Roles('admin')
   @Get('assignor/:id')
   async findOneAssignor(@Param('id') id: string) {
     return await this.assignorService.findOne(id);
   }
 
+  @Roles('admin')
   @Patch('assignor/:id')
   async updateAssignor(
     @Param('id') id: string,
@@ -41,21 +46,25 @@ export class AssignorController {
     return await this.assignorService.update(id, updateAssignorDto);
   }
 
+  @Roles('admin')
   @Delete('assignor/:id')
   async deleteAssignor(@Param('id') id: string) {
     return await this.assignorService.remove(id);
   }
 
+  @Roles('admin')
   @Get('assignor/recover/all')
   async listDeletedAssignors() {
     return await this.assignorService.listDeletedAssignors();
   }
 
+  @Roles('admin')
   @Get('assignor/recover/:id')
   async findDeletedAssignor(@Param('id') id: string) {
     return await this.assignorService.findDeletedAssignor(id);
   }
 
+  @Roles('admin')
   @Patch('assignor/recover/:id')
   async recoverAssignor(@Param('id') id: string) {
     return await this.assignorService.recoverAssignor(id);
