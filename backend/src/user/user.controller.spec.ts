6b28b516baff70e3d41bfce920044b3937bf0f2c
Gabriel Muniz <gabrielmfd@gmail.com>
2025-03-24 19:26:37 -0300

    feat(backend): finish users controller test implementation

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -5,6 +5,7 @@ import { PrismaService } from 'src/prisma_service/prisma.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +14,44 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('user controller', () => {
+    it('Should find all users', async () => {
+      const mockResponse = [
+        {
+          id: '7c9a1eb0-20e0-4f4f-a62a-8c459b6385d2',
+          login: 'aprovame',
+          password: 'aprovame',
+          role: 'admin',
+        },
+      ];
+
+      jest.spyOn(service, 'findAll').mockResolvedValue(mockResponse);
+
+      const response = await controller.findAll();
+
+      expect(response).toEqual(mockResponse);
+    });
+
+    it('Should create a user', async () => {
+      const request = {
+        login: 'aprovame',
+        password: 'aprovame',
+      };
+
+      const serviceReponse = { message: 'Usuário cadastrado com sucesso!' };
+
+      jest.spyOn(service, 'create').mockResolvedValue(serviceReponse);
+
+      const response = await controller.createUser(request);
+
+      expect(response).toEqual(serviceReponse);
+    });
   });
 });
